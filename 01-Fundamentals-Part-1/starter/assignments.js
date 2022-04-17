4069924825789f24035a7d219f4f5d2f5ffe4336
ReltoH <hectormgallardos@gmail.com>
2022-04-16 23:44:15 -0700

    completed data types

--- 01-Fundamentals-Part-1/starter/assignments.js
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -8,5 +8,12 @@ console.log(continent);
 console.log(population);
 */
 
-/*LECTURE: Data Types*/
+/*LECTURE: Data Types
 let isIsland = false;
+let language;
+let country = 'Mexico';
+let population = 131;
+console.log(typeof isIsland);
+console.log(typeof language);
+console.log(typeof country);
+console.log(typeof population);*/
