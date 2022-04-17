044de9a2b8d0cf19332c2f65c5d1f590899369d2
ReltoH <hectormgallardos@gmail.com>
2022-04-17 16:07:29 -0700

    Completed variables

--- 01-Fundamentals-Part-1/starter/assignments.js
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -1,19 +1,22 @@
-/* LECTURE: Values and Variables
-let country = 'Mexico';
-let continent = 'America';
+/* //LECTURE: Values and Variables
+const country = 'Mexico';
+const continent = 'America';
 let population = 131;
 
 console.log(country);
 console.log(continent);
 console.log(population);
-*/
 
-/*LECTURE: Data Types
-let isIsland = false;
+
+//LECTURE: Data Types
+const isIsland = false;
 let language;
-let country = 'Mexico';
-let population = 131;
 console.log(typeof isIsland);
 console.log(typeof language);
 console.log(typeof country);
-console.log(typeof population);*/
+console.log(typeof population);
+
+//LECTURE: let, const and var
+language = "Spanish";
+*/
+
