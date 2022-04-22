33157d05b3c15a8b7fe396de19f95d0ec6d1eefb
ReltoH <hectormgallardos@gmail.com>
2022-04-21 21:13:20 -0700

    Completed challenge 3

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -54,3 +54,46 @@ if (markBMI1 > jhonBMI1) {
         console.log(`Mark's and Jhon's BMI is the same (${jhonBMI1})`);
     }
 } */
+
+//Coding Challenge #3
+const scoreDolphin1 = (96 + 108 + 89) / 3;
+const scoreDolphin2 = (97 + 112 + 101) / 3;
+const scoreDolphin3 = (97 + 112 + 101) / 3;
+
+const scoreKoalas1 = (88 + 91 + 110) / 3;
+const scoreKoalas2 = (109 + 95 + 123) / 3;
+const scoreKoalas3 = (109 + 95 + 106) / 3;
+
+//First score
+console.log("First score: " + scoreDolphin1 + ' ' + scoreKoalas1);
+if (scoreDolphin1 > scoreKoalas1 && scoreDolphin1 >= 100) {
+    console.log(`The Dolphin team wins the trophy with a score of ${scoreDolphin1}.`);
+} else if (scoreDolphin1 < scoreKoalas1 && scoreDolphin1 >= 100) {
+    console.log(`The Koalas team wins the trophy with a score of ${scoreKoalas1}.`);
+} else if (scoreDolphin1 === scoreKoalas1 && scoreDolphin1 >= 100) {
+    console.log(`There is a draw with a score of ${scoreDolphin1}.`);
+} else {
+    console.log('No team wins the trophy.');
+}
+//Second score
+console.log("Second score: " + scoreDolphin2 + ' ' + scoreKoalas2);
+if (scoreDolphin2 > scoreKoalas2 && scoreDolphin2 >= 100) {
+    console.log(`The Dolphin team wins the trophy with a score of ${scoreDolphin2}.`);
+} else if (scoreDolphin2 < scoreKoalas2 && scoreDolphin2 >= 100) {
+    console.log(`The Koalas team wins the trophy with a score of ${scoreKoalas2}.`);
+} else if (scoreDolphin2 === scoreKoalas2 && scoreDolphin2 >= 100) {
+    console.log(`There is a draw with a score of ${scoreDolphin2}.`);
+} else {
+    console.log('No team wins the trophy.');
+}
+//Third score
+console.log("Third score: " + scoreDolphin3 + ' ' + scoreKoalas3);
+if (scoreDolphin3 > scoreKoalas3 && scoreDolphin3 >= 100) {
+    console.log(`The Dolphin team wins the trophy with a score of ${scoreDolphin3}.`);
+} else if (scoreDolphin3 < scoreKoalas3 && scoreDolphin3 >= 100) {
+    console.log(`The Koalas team wins the trophy with a score of ${scoreKoalas3}.`);
+} else if (scoreDolphin3 === scoreKoalas3 && scoreDolphin3 >= 100) {
+    console.log(`There is a draw with a score of ${scoreDolphin3}.`);
+} else {
+    console.log('No team wins the trophy.');
+}
