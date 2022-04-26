59b03f7e17d634beb543dd26227257d9dfedf38d
Relto <hectormgallardos@gmail.com>
2022-04-25 21:20:30 -0700

    Finished Arrow functions

---
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -0,0 +1,29 @@
+'use strict';
+/* // LECTURE: Functions
+function describeCountry(country, population, capitalCity) {
+    const description = `${country} has ${population} million people and its capital city is ${capitalCity}`;
+    return description;
+}
+const firstCall = describeCountry('Mexico', 130, 'CDMX');
+const secondCall = describeCountry('Canada', 30, 'Otawa');
+const thirdCall = describeCountry('USA', 350, 'Washington');
+
+console.log(firstCall);
+console.log(secondCall);
+console.log(thirdCall); */
+
+/* //LECTURE: Function Declarations vs. Expressions
+function percentageOfWorld1(population) {
+    return population / 7900 * 100;
+}
+
+let percentageOfWorld2 = function (population) {
+    return population / 7900 * 100;
+}
+
+console.log(percentageOfWorld1(300), percentageOfWorld1(50), percentageOfWorld1(5000));
+console.log(percentageOfWorld2(500), percentageOfWorld2(30), percentageOfWorld2(3000)); */
+
+/* // LECTURE: Arrow Functions
+const percentageOfWorld3 = population => population / 7900 * 100;
+console.log(percentageOfWorld3(3000)); */
