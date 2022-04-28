1f2a7959266b41ab755e8980152c512c36aa5569
Relto <hectormgallardos@gmail.com>
2022-04-27 21:19:38 -0700

    Finally objects are next

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -42,4 +42,60 @@ console.log(describePopulation('Mexico', 350));
 console.log(describePopulation('Canada', 5000));
 console.log(describePopulation('Peru', 1000)); */
 
-// Coding Challenge #1
+/* // Coding Challenge #1
+const calcAverage = (score1, score2, score3) => (score1 + score2 + score3) / 3;
+function checkWinner(scoreDolphins, scoreKoalas) {
+    if (scoreDolphins >= (scoreKoalas * 2)) {
+        return `Dolphins win (${scoreDolphins} vs. ${scoreKoalas})`;
+    } else if (scoreKoalas >= (scoreDolphins * 2)) {
+        return `Koalas win (${scoreKoalas} vs. ${scoreDolphins})`;
+    }
+}
+// const avgDolphins = calcAverage(44, 23, 71);
+// const avgKoalas = calcAverage(65, 54, 49);
+
+const avgDolphins = calcAverage(85, 54, 41);
+const avgKoalas = calcAverage(23, 34, 27);
+
+console.log(avgDolphins, avgKoalas);
+console.log(checkWinner(avgDolphins, avgKoalas)); */
+
+/* //LECTURE: Introduction to Arrays
+const populations = [300, 5000, 3000, 40];
+console.log(populations.length == 4);
+
+function percentageOfWorld1(population) {
+    return population / 7900 * 100;
+}
+const percentages = [percentageOfWorld1(populations[0]), percentageOfWorld1(populations[1]), percentageOfWorld1(populations[2]), percentageOfWorld1(populations[populations.length - 1])];
+console.log(percentages); */
+
+/* //LECTURE: Basic Array Operations (Methods)
+const neighbours = ['USA', 'Guatemala', 'Belice'];
+console.log(neighbours);
+neighbours.push('Utopia');
+console.log(neighbours);
+neighbours.pop();
+console.log(neighbours);
+
+!neighbours.includes('Germany') ? console.log('Probably not a central European Country :D') : console.log(false);
+
+neighbours[neighbours.indexOf('USA')] = 'United States of America';
+console.log(neighbours); */
+
+/* //Coding Challenge #2
+function calcTip(bill) {
+    if (bill >= 50 && bill <= 300) {
+        return bill * .15;
+    } else {
+        return bill * .2;
+    }
+}
+console.log(calcTip(100));
+const bills = [125, 555, 44];
+const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
+const totals = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]];
+
+console.log(bills);
+console.log(tips);
+console.log(totals); */
