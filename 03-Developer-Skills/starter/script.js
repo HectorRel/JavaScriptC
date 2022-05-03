ce3daf4a6dc32c43d01468dcff4242f16ed85e5a
Relto <hectormgallardos@gmail.com>
2022-05-02 21:13:28 -0700

    HTML CSS crash course!

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -1,7 +1,7 @@
 // Remember, we're gonna use strict mode in all scripts now!
 "use strict";
 
-const tempertures = [3, -2, -6, -1, 'error', 9, 13, 17, 15, 14, 9, 5];
+/* const tempertures = [3, -2, -6, -1, 'error', 9, 13, 17, 15, 14, 9, 5];
 
 const tempAmplitude = function (termpertures) {
     let minTemp = tempertures[0];
@@ -20,4 +20,21 @@ const tempAmplitude = function (termpertures) {
     return maxTemp - Math.abs(minTemp);
 }
 
-console.log(tempAmplitude(tempertures));
+console.log(tempAmplitude(tempertures)); */
+
+// Coding Challenge #1
+const temps1 = [17, 21, 23];
+const temps2 = [12, 5, -5, 0, 4];
+
+const printForecast = function (temp) {
+    const count = temp.length;
+    let i = 0;
+    let forecastString = `...${temp[i]} C in ${i + 1} days`;
+
+    for (i = 1; i < count; i++) {
+        forecastString += `...${temp[i]} C in ${i + 1} days...`;
+    }
+    console.log(forecastString);
+}
+printForecast(temps1);
+printForecast(temps2);
