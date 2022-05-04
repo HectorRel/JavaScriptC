d043e9b8544e90ac3bb8e3450bd2fa0e7ba0105c
Relto <hectormgallardos@gmail.com>
2022-05-03 21:24:51 -0700

    Here we start rolling!

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,1 +1,19 @@
 'use strict';
+/*const message = document.querySelector('.message').textContent;
+console.log(message);
+document.querySelector('.message').textContent = 'holi crayoli';
+document.querySelector('.number').textContent = '20';
+console.log(document.querySelector('body').style.backgroundColor = 'green');
+
+const input = document.querySelector('.guess').value;
+console.log(input); */
+
+document.querySelector('.check').addEventListener('click', function() {
+    const guess = Number(document.querySelector('.guess').value);
+    console.log(typeof guess, guess);
+
+    if (guess === 15){
+        document.querySelector('.message').textContent = 'Correct number!!!!';
+        document.querySelector('body').style.backgroundColor = 'green';
+    }
+});
