951d740b11c3801b1af5ad6aec1325cdc745c796
Relto <hectormgallardos@gmail.com>
2022-05-04 21:01:12 -0700

    Finished guess my number game

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,19 +1,43 @@
 'use strict';
-/*const message = document.querySelector('.message').textContent;
-console.log(message);
-document.querySelector('.message').textContent = 'holi crayoli';
-document.querySelector('.number').textContent = '20';
-console.log(document.querySelector('body').style.backgroundColor = 'green');
+const randomizeNum = function() {
+    return Math.round(Math.random()*20);
+}
+let numToGuess = randomizeNum();
+let score = Number (document.querySelector('.score').textContent);
+let highscore = Number (document.querySelector('.highscore').textContent);
+console.log(numToGuess);
 
-const input = document.querySelector('.guess').value;
-console.log(input); */
-
+//Managing Check boton
 document.querySelector('.check').addEventListener('click', function() {
     const guess = Number(document.querySelector('.guess').value);
-    console.log(typeof guess, guess);
-
-    if (guess === 15){
+    if (guess === numToGuess){
         document.querySelector('.message').textContent = 'Correct number!!!!';
         document.querySelector('body').style.backgroundColor = 'green';
+        document.querySelector('.number').textContent = `${numToGuess}`;
+        if(score > highscore) {
+            highscore = score;
+            document.querySelector('.highscore').textContent = `${highscore}`;
+        }
+    } else if(guess > numToGuess && guess <= 20) {
+        document.querySelector('.message').textContent = 'Too high!';
+        score -= 1;
+    } else if(guess < numToGuess && guess <= 20) {
+        document.querySelector('.message').textContent = 'Too low!';
+        score -= 1;
+    } else {
+        document.querySelector('.message').textContent = 'Between 1 and 20'
     }
+    document.querySelector('.score').textContent = `${score}`;
+});
+
+//Managing Again boton
+document.querySelector('.again').addEventListener('click', function() {
+    numToGuess = randomizeNum();
+    score = 20;
+    console.log(numToGuess);
+    document.querySelector('body').style.backgroundColor = '#222';
+    document.querySelector('.message').textContent = 'Start guessing...';
+    document.querySelector('.guess').value = '';
+    document.querySelector('.number').textContent = '?';
+    document.querySelector('.score').textContent = '20';
 });
